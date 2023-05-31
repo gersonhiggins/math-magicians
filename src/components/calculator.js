a9ca4d9c7be6fe12684af87312074459ccae4199
gersonhiggins <gahg.10gerson@gmail.com>
2023-05-31 13:07:07 -0400

    added functionality to calc

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  numberClick, operatorClick, ac, dot, result, porcentaje,
+} from '../modules/calculate';
 
 const Calculator = () => (
   <div className="calculator">
@@ -6,27 +9,28 @@ const Calculator = () => (
     <div className="calculator-box">
       <form>
         <div className="grid">
-          <input type="text" value="0" readOnly />
+          <input className="input" type="text" value="0" readOnly />
+          <p className="saved">0</p>
           <div className="buttons">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="calc">+</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="calc">x</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="calc">-</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="calc">+</button>
-            <button type="button" className="cero">0</button>
-            <button type="button">.</button>
-            <button type="button" className="calc">=</button>
+            <button onClick={ac} type="button" className="ac">AC</button>
+            <button onClick={operatorClick} type="button" className="operator">+/-</button>
+            <button onClick={porcentaje} type="button" className="operator">%</button>
+            <button onClick={operatorClick} type="button" className="calc operator">÷</button>
+            <button onClick={numberClick} type="button" className="number">7</button>
+            <button onClick={numberClick} type="button" className="number">8</button>
+            <button onClick={numberClick} type="button" className="number">9</button>
+            <button onClick={operatorClick} type="button" className="calc operator">x</button>
+            <button onClick={numberClick} type="button" className="number">4</button>
+            <button onClick={numberClick} type="button" className="number">5</button>
+            <button onClick={numberClick} type="button" className="number">6</button>
+            <button onClick={operatorClick} type="button" className="calc operator">-</button>
+            <button onClick={numberClick} type="button" className="number">1</button>
+            <button onClick={numberClick} type="button" className="number">2</button>
+            <button onClick={numberClick} type="button" className="number">3</button>
+            <button onClick={operatorClick} type="button" className="calc operator">+</button>
+            <button onClick={numberClick} type="button" className="number cero">0</button>
+            <button onClick={dot} type="button" className="number">.</button>
+            <button onClick={result} type="button" className="calc result">=</button>
           </div>
         </div>
       </form>
